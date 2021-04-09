023024b9baf317298aef48001bfee94ebeb10c01
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-04-09 16:57:34 -0600

    initializing plugin register

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-
+import pkg from "../package.json";
+import {PACKAGE_NAME} from "./config/constants.js";
 export default async function register(app){
-    
+    app.registerPlugin({
+        label:"Shipment rate plugin",
+        name:PACKAGE_NAME,
+        version:pkg.version
+    })
 }
